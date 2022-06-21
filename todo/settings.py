36d9d9cf9197eb1b47ae5f91df3cbc27a25f9fad
anil-bothe <anil.bothe.patil@gmail.com>
2022-06-21 20:55:14 +0530

    to added

--- todo/settings.py
+++ todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-58rwd&-u!8$x&9&bhf##j@&o8!#e9(1u+88!o1ckzvtfy6iu5o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = "/home/anilbothe/todo/static_root/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
